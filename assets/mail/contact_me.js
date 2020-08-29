c1d9e1a7e5f7354a0e8ba3df7d16b5ec02e0c3a8
Kang se eung <seeung0305@naver.com>
2020-08-30 01:25:32 +0900

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,8 +21,8 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                // url: "/assets/mail/contact_me.php",
-                url: 'https://seeungkang.github.io/portfolio/assets/mail/contact_me.php',
+                url: "/assets/mail/contact_me.php",
+                // url: 'https://seeungkang.github.io/portfolio/assets/mail/contact_me.php',
                 type: "POST",
                 data: {
                     name: name,
